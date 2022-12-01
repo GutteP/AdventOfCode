bcec4d7acb6e37a9d30d333198cf6e8320def135
Gustav Pettersson <gustav.b.pettersson@gmail.com>
2022-12-01 06:45:12 +0100

    Day 1

---
+++ AoC.2022.Test/Day1Tests.cs
@@ -0,0 +1,25 @@
+﻿using AoC._2022.Day1;
+
+namespace AoC._2022.Test;
+
+public class Day1ToXTests
+{
+    [Theory]
+    [InlineData("Day1/t1.txt", 24000, 45000)]
+    [InlineData("Day1/input.txt", 67658, 200158)]
+
+    public void DayOneTest(string path, int? expectedPartOne, int? expectedPartTwo)
+    {
+        new CalorieCounter().Test(path, expectedPartOne, expectedPartTwo);
+    }
+
+    [Theory]
+    [InlineData("Day2/t1.txt", 1, null)]
+    [InlineData("Day2/input.txt", null, null)]
+
+    public void DayTwoTest(string path, int? expectedPartOne, int? expectedPartTwo)
+    {
+        //new CalorieCounter().Test(path, expectedPartOne, expectedPartTwo);
+    }
+
+}
---
+++ AoC.2022/Day1/CalorieCounter.cs
@@ -0,0 +1,34 @@
+﻿namespace AoC._2022.Day1;
+
+public class CalorieCounter : IAoCDay
+{
+    public DayRunner Runner()
+    {
+        return new DayRunner(new Runner<List<int>, int>(Transformer, Solver, 1), new Runner<List<int>, int>(Transformer, Solver, 3));
+    }
+
+    private List<int> Transformer(string path)
+    {
+        List<int> result = new();
+
+        List<int> nisse = new();
+        foreach (string row in InputReader.ReadLines(path))
+        {
+            if (string.IsNullOrEmpty(row))
+            {
+                result.Add(nisse.Sum());
+                nisse = new();
+                continue;
+            }
+            nisse.Add(int.Parse(row));
+        }
+        if (nisse.Any()) result.Add(nisse.Sum());
+
+        return result;
+    }
+
+    private int Solver(List<int> input, int numberToCount)
+    {
+        return input.OrderByDescending(x => x).ToArray()[0..numberToCount].ToList().Sum();
+    }
+}
