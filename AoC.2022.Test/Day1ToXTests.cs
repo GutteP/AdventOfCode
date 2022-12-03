5095168c4357e145cd1db207b2c899e6759314f4
Gustav Pettersson <gustav.b.pettersson@gmail.com>
2022-12-03 08:46:15 +0100

    Day 3

--- AoC.2022.Test/Day1ToXTests.cs
+++ AoC.2022.Test/Day1ToXTests.cs
@@ -1,5 +1,6 @@
 ﻿using AoC._2022.Day1;
 using AoC._2022.Day2;
+using AoC._2022.Day3;
 
 namespace AoC._2022.Test;
 
@@ -15,12 +16,20 @@ public class Day1ToXTests
     }
 
     [Theory]
-    [InlineData("Day2/t1.txt", 15, 12)]
+    [InlineData("Day2/t1.txt", 15, 2752)]
     [InlineData("Day2/input.txt", 14069, 12411)]
 
     public void DayTwoTest(string path, int? expectedPartOne, int? expectedPartTwo)
     {
         new RPSScoreCalculator().Test(path, expectedPartOne, expectedPartTwo);
+    }
+    [Theory]
+    [InlineData("Day3/t1.txt", 157, 70)]
+    [InlineData("Day3/input.txt", 7821, 2752)]
+
+    public void DayThreeTest(string path, int? expectedPartOne, int? expectedPartTwo)
+    {
+        new RucksackOrganaizer().Test(path, expectedPartOne, expectedPartTwo);
     }
 
 }
