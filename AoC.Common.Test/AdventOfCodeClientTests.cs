28f9783fadc8622c02584553482049064d643be2
Gustav Pettersson <gustav.b.pettersson@gmail.com>
2023-12-05 15:12:16 +0100

    Client and Configuration

---
+++ AoC.Common.Test/AdventOfCodeClientTests.cs
@@ -0,0 +1,11 @@
+﻿namespace AoC.Common.Test;
+
+public class AdventOfCodeClientTests
+{
+    [Fact]
+    public async Task GetInputTest()
+    {
+        AdventOfCodeClient client = new(Configuration.Session);
+        string input = await client.GetInput(2023, 5);
+    }
+}
---
+++ AoC.Common/AdventOfCodeClient.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace AoC.Common;
+
+public class AdventOfCodeClient
+{
+    HttpClient _httpClient;
+    public AdventOfCodeClient(string session)
+    {
+        _httpClient = new HttpClient();
+        _httpClient.BaseAddress = new Uri("https://adventofcode.com/");
+        _httpClient.DefaultRequestHeaders.Add("Cookie", "session=" + session);
+    }
+
+    public async Task<string> GetInput(int year, int day)
+    {
+        //2023/day/5/input
+        HttpResponseMessage response = await _httpClient.GetAsync($"{year}/day/{day}/input");
+        return await response.Content.ReadAsStringAsync();
+    }
+}
